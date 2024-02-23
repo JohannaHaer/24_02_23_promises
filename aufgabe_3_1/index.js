b5a9cf113554b2c5ea95e4e59c74d5d8c94c2cbf
JohannaHaer <johanna.haerdter@gmail.com>
2024-02-23 16:52:18 +0100

    aufgabe 3.1

---
+++ aufgabe_3_1/index.js
@@ -0,0 +1,21 @@
+const newPromise = (num) => {
+    return new Promise((resolve, reject) => {
+        let newNum = num * num
+        resolve(newNum)
+    })
+}
+
+const rounds = (num) => {
+    newPromise(num).then((result) => {
+        console.log(result);
+        return newPromise(result);
+    }).then((result) => {
+        console.log(result);
+        return newPromise(result);
+    }).then((result) => {
+        console.log(result);
+        return newPromise(result);
+    })
+}
+
+rounds(3)
